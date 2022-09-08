3ed0f9ddf2d1a580e1657f493fefad70640c6e2e
Chrissy LeMaire <clemaire@gmail.com>
2022-09-08 23:46:13 +0200

    a

--- public/Install-PwPlaywright.ps1
+++ public/Install-PwPlaywright.ps1
@@ -5,7 +5,7 @@ function Install-PwPlaywright {
     if ($isLinux -or $isMac) {
         Write-Verbose "installing on linux or mac $script:npxpath"
         #$exec1 = Invoke-Program -FilePath $script:npmpath -ArgumentList "install" -ErrorAction Stop -Verbose -WorkingDirectory $script:root
-        $exec = Invoke-Program -FilePath $script:npxpath -ArgumentList "--prefix $script:modulebin --location=global -y playwright install" -ErrorAction Stop
+        $exec = Invoke-Program -FilePath $script:npxpath -ArgumentList "--prefix $script:modulebin -y playwright install" -ErrorAction Stop
 
         if ($exec.ExitCode -ne 0) {
             $exec
