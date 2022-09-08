6797edadb8c54fee327926635d217546e35f19eb
Chrissy LeMaire <clemaire@gmail.com>
2022-09-08 10:21:31 +0200

    Initial commit
    updates

---
+++ psplaywright.psm1
@@ -0,0 +1,15 @@
+﻿
+$dir = Join-Path $PSScriptRoot bin
+$null = Add-Type -Path (Join-Path -Path $dir -ChildPath Microsoft.Playwright.dll)
+
+function playwright {
+    [Microsoft.Playwright.Program]::Main("$args")
+}
+function Install-PwPlaywright {
+    $exec = playwright install
+    if ($exec -ne 0) {
+        throw "Failed to install Playwright"
+    } else {
+        Write-Output "Playwright installed successfully"
+    }
+}
